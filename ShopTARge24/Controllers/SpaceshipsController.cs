38eb61014d076d8a674f51d4a0f749e4d5c8363f
Opilane TTHK <122533981+JelenaPenkina@users.noreply.github.com>
2025-09-02 11:40:02 +0300

    Spaceships Data and Domain
    Spaceships Data,Domain and ControllerView

--- ShopTARge24/Controllers/SpaceshipsController.cs
+++ ShopTARge24/Controllers/SpaceshipsController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge24.Data;
+using ShopTARge24.Models.Spaceships;
 
 namespace ShopTARge24.Controllers
 {
     public class SpaceshipsController : Controller
     {
+        private readonly ShopTARge24Context _context;
+        public SpaceshipsController
+            (
+                ShopTARge24Context context
+            )
+        { 
+            _context = context;
+        }
+
         public IActionResult Index()
+
         {
-            return View();
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Classification = x.Classification,
+                    BuiltDate = x.BuiltDate
+                });
+
+            return View(result);
         }
     }
 }
---
+++ ShopTARge24/Models/Spaceships/SpaceshipIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopTARge24.Models.Spaceships
+{
+    public class SpaceshipIndexViewModel
+    {
+        public Guid? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Classification { get; set; }
+
+        public DateTime? BuiltDate { get; set; }
+
+        public int? Crew { get; set; }
+
+        public int? EnginePower { get; set; }
+    }
+}
