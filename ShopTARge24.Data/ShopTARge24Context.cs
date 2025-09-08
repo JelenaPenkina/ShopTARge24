89cde7ed44a90639fbb4743a8c5a5f45c8a9a839
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-09-08 18:14:35 +0300

    Data Server
    Data serveri ühenduse loomine

--- ShopTARge24.Data/ShopTARge24Context.cs
+++ ShopTARge24.Data/ShopTARge24Context.cs
@@ -6,6 +6,9 @@ namespace ShopTARge24.Data
 {
     public class ShopTARge24Context : DbContext
     {
+
+        public ShopTARge24Context(DbContextOptions<ShopTARge24Context> options)
+            : base(options) { }
         public DbSet<Spaceships> Spaceships { get; set; }
     }
 }
