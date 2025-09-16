d721646a29edee71935b1b52d134250112b60e33
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-09-16 11:38:13 +0300

    Update DetailsViewModel
    Added two models

--- ShopTARge24/Models/Spaceships/SpaceshipDetailsViewModel.cs
+++ ShopTARge24/Models/Spaceships/SpaceshipDetailsViewModel.cs
@@ -5,11 +5,14 @@
         public Guid? Id { get; set; }
         public string? Name { get; set; }
         public string? Classification { get; set; }
+        public string? TypeName { get; set; }
+        public string? SpaceshipModel { get; set; }
         public DateTime? BuiltDate { get; set; }
         public int? Crew { get; set; }
         public int? EnginePower { get; set; }
 
         public DateTime? CreatedAt { get; set; }
         public DateTime? ModifiedAt { get; set; }
+
     }
 }
