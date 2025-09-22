f93f8ab47a73869179eaecff549d47fb5db1c43f
Opilane TTHK <122533981+JelenaPenkina@users.noreply.github.com>
2025-09-22 18:09:28 +0300

    FileServices
    FileServices

---
+++ ShopTARge24.ApplicationServices/Services/FileServices.cs
@@ -0,0 +1,54 @@
+﻿using ShopTARge24.Core.Domain;
+using ShopTARge24.Core.Dto;
+using ShopTARge24.Core.ServiceInterface;
+using Microsoft.Extensions.Hosting;
+using ShopTARge24.Data;
+
+namespace ShopTARge24.ApplicationServices.Services
+{
+    public class FileServices : IFileServices
+    {
+        private readonly IHostEnvironment _webHost;
+        private readonly ShopTARge24Context _context;
+        public FileServices
+            (
+            IHostEnvironment webHost, 
+            ShopTARge24Context context
+            )
+        {
+            _webHost = webHost;
+            _context = context;
+        }
+        public void FilesToApi(SpaceshipDto dto, Spaceships domain)
+        {
+            if (dto.Files != null && dto.Files.Count > 0)
+            {
+                if (!Directory.Exists(_webHost.ContentRootPath +"\\multipleFileUpload\\"))
+                {
+                    Directory.CreateDirectory(_webHost.ContentRootPath + "\\multipleFileUpload\\");
+                }
+
+                foreach (var file in dto.Files) 
+                {
+                    string uploadsFolder = Path.Combine(_webHost.ContentRootPath, "multipleFileUpload");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName); // annab unique nime file, et neid eristada 
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create)) // uurida võrgu teemat, sest seda jagatakse väiksemateks tükkideks, et saan korraga mitu inimest teha
+                    {
+                        file.CopyTo(fileStream);
+
+                        FileToApi path = new FileToApi
+                        {
+                            Id = Guid.NewGuid(),
+                            ExistingFilePath = uniqueFileName, 
+                            SpaceshipId = domain.Id
+                        };
+
+                        _context.FileToApis.AddAsync(path);
+                    }
+                }
+            }
+        }
+    }
+}
