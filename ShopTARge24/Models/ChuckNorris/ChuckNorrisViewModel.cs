b18db7200e8a215c6254542d2f12dba07adf8c6c
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-10-21 16:18:10 +0300

    ChuckNorris View
    ViewModal, index vaate tegemine

---
+++ ShopTARge24/Models/ChuckNorris/ChuckNorrisViewModel.cs
@@ -0,0 +1,17 @@
+﻿using
+
+using ShopTARge24.Core.Dto;
+
+namespace ShopTARge24.Models.ChuckNorris
+{
+    public class ChuckNorrisViewModel
+    {
+        public List<Categories> Categories;
+        public string? id { get; set; }
+        public string? iconUrl { get; set; }
+        public string? url { get; set; }
+        public string? value { get; set; }
+        public string? createdAt { get; set; }
+        public string? updatedAt { get; set; }
+    }
+}
