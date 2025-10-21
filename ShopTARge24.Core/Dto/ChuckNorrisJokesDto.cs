641a6e70f4577ec9ccfa79f1d7f2b11f474a0b52
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-10-21 16:06:37 +0300

    Weather FinishLine ja ChuckNorris
    apiKey muutmine ning ChuckNorrise Dto lisamine

---
+++ ShopTARge24.Core/Dto/ChuckNorrisJokesDto.cs
@@ -0,0 +1,24 @@
+﻿namespace ShopTARge24.Core.Dto
+{
+    class ChuckNorrisJokesDto
+    {
+
+        public List<Categories> Categories;
+        public string? id { get; set; }
+        public string? iconUrl { get; set; }
+        public string? url { get; set; }
+        public string? value { get; set; }
+        public string? createdAt { get; set; }
+        public string? updatedAt { get; set; }
+    }
+
+    public class Categories
+    {
+        public string? id {  get; set; }
+        public string? iconUrl { get; set; }
+        public string? url { get; set; }
+        public string? value { get; set; }
+        public string? createdAt { get; set; }
+        public string? updatedAt { get; set; }
+    }
+}
