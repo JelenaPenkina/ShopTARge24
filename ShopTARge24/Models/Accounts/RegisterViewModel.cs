4005bac802ba4183d7f10dcdfeb0954f3be2ad25
Opilane TTHK <122533981+JelenaPenkina@users.noreply.github.com>
2025-12-08 11:06:36 +0200

    Accounts Register loomine
    Kasutades EmailServise classi Accounts-Register jaoks,

---
+++ ShopTARge24/Models/Accounts/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ShopTARge24.Models.Accounts
+{
+    public class RegisterViewModel
+    {
+        [Required]
+       // [ValidEmailDomain()]
+        [EmailAddress]       
+        public string Email { get; set; }
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        public string City { get; set; }
+
+    }
+}
