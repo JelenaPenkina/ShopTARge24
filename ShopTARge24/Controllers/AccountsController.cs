3fbc2c6a099004038c6ba68e1114e96e0735b956
Opilane TTHK <122533981+JelenaPenkina@users.noreply.github.com>
2025-12-16 11:28:00 +0200

    Forgot ja Reset Views

--- ShopTARge24/Controllers/AccountsController.cs
+++ ShopTARge24/Controllers/AccountsController.cs
@@ -228,6 +228,41 @@ namespace ShopTARge24.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+           if(!ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null && await _userManager.IsEmailConfirmedAsync(user))
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    var passwordResetLink = Url.Action("ResetPassword", "Accounts", new { email = model.Email, token = token }, Request.Scheme);
+
+                    var emailDto = new EmailDto
+                    {
+                        To = model.Email,
+                        Subject = "Reset your password",
+                        Body = $"Please reset your password by clicking <a href='{passwordResetLink}'>here</a>"
+                    };
+
+                    _emailServices.SendEmail(emailDto);
+                    return View("ForgotPasswordConfirmation");
+                }
+                return View("ForgotPasswordConfirmation");
+            }
+
+            return View(model);
+        }
     }
 }
  
---
+++ ShopTARge24/Models/Accounts/ResetPasswordViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopTARge24.Models.Accounts
+{
+    public class ResetPasswordViewModel
+    {
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        public string Token { get; set; }
+    }
+}
